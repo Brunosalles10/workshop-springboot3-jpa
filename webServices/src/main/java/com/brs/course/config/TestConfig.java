7f582251040e8d9ac226615db4ec419183a1d2bd
Bruno Salles <bsalles428@gmail.com>
2025-02-09 20:35:12 -0300

    Product - ordemItem

--- webServices/src/main/java/com/brs/course/config/TestConfig.java
+++ webServices/src/main/java/com/brs/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.brs.course.entities.Category;
 import com.brs.course.entities.Order;
+import com.brs.course.entities.OrderItem;
 import com.brs.course.entities.Product;
 import com.brs.course.entities.User;
 import com.brs.course.entities.enums.OrderStatus;
 import com.brs.course.repositeres.CategoryRepository;
+import com.brs.course.repositeres.OrderItemRepository;
 import com.brs.course.repositeres.OrderRepository;
 import com.brs.course.repositeres.ProductRepository;
 import com.brs.course.repositeres.UserRepository;
@@ -24,6 +26,9 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private OrderItemRepository orderItemRepository;
+	
 	@Autowired
 	private OrderRepository orderRepository;
 
@@ -46,17 +51,16 @@ public class TestConfig implements CommandLineRunner {
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 
-	
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
-		
+
 		p1.getCategories().add(cat2);
 		p2.getCategories().add(cat1);
 		p2.getCategories().add(cat3);
 		p3.getCategories().add(cat3);
 		p4.getCategories().add(cat3);
 		p5.getCategories().add(cat2);
-		
+
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
@@ -68,6 +72,13 @@ public class TestConfig implements CommandLineRunner {
 
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
 	}
 
---
+++ webServices/src/main/java/com/brs/course/repositeres/OrderItemRepository.java
@@ -0,0 +1,13 @@
+package com.brs.course.repositeres;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.brs.course.entities.Category;
+import com.brs.course.entities.OrderItem;
+
+
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+	
+
+}
